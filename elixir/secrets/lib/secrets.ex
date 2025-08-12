dd0bacadd857a29ff455b974bd5260215c7c5b62
Sokoshy <92679596+Sokoshy@users.noreply.github.com>
2025-08-12 19:27:46 +0200

    refacto exercice secrets

--- elixir/secrets/lib/secrets.ex
+++ elixir/secrets/lib/secrets.ex
@@ -1,43 +1,37 @@
 defmodule Secrets do
   def secret_add(secret) do
     # Please implement the secret_add/1 function
-    adder = fn number -> secret + number end
-    adder
+    fn number -> secret + number end
   end
 
   def secret_subtract(secret) do
     # Please implement the secret_subtract/1 function
-    subtractor = fn number -> number - secret end
-    subtractor
+    fn number -> number - secret end
   end
 
   def secret_multiply(secret) do
     # Please implement the secret_multiply/1 function
-    multiplier = fn number -> number * secret end
-    multiplier
+    fn number -> number * secret end
   end
 
   def secret_divide(secret) do
     # Please implement the secret_divide/1 function
-    divider = fn number -> Integer.floor_div(number, secret) end
-    divider
+    fn number -> Integer.floor_div(number, secret) end
   end
 
   def secret_and(secret) do
     # Please implement the secret_and/1 function
-    ander = fn number -> Bitwise.band(secret, number) end
-    ander
+    fn number -> Bitwise.band(secret, number) end
   end
 
   def secret_xor(secret) do
     # Please implement the secret_xor/1 function
-    xorer = fn number -> Bitwise.bxor(number, secret) end
-    xorer
+    fn number -> Bitwise.bxor(number, secret) end
   end
 
   def secret_combine(secret_function1, secret_function2) do
     # Please implement the secret_combine/2 function
-    combined = fn number ->
+    fn number ->
       result1 = secret_function1.(number)
       secret_function2.(result1)
     end
